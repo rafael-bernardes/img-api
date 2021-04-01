1bad9636d31ec3b1c45adf69a6e978d023942590
Rafael Pereira <rafael.bernardes@live.com>
2021-04-01 10:03:15 -0300

    Renomeia pacote

---
+++ src/main/java/br/gov/bom_destino/img/api/rest/DadosGeograficosResource.java
@@ -0,0 +1,29 @@
+package br.gov.bom_destino.img.api.rest;
+
+import java.io.Serializable;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+
+@Path("dados-geograficos")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class DadosGeograficosResource implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private ResteasyClient client;
+	
+	@GET
+	public Response getIBGEData() {
+		return Response.ok("funcionou!").build();
+	}
+}
